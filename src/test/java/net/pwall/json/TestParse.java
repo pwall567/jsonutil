221fdb9aa56a1118f0bdd835cf46330e0aa27030
Peter Wall <pwall@pwall.net>
2017-02-20 14:07:29 +1100

    Minor additions to JUnit tests

--- src/test/java/net/pwall/json/TestParse.java
+++ src/test/java/net/pwall/json/TestParse.java
@@ -11,14 +11,12 @@ import java.io.StringReader;
 
 import org.junit.Test;
 
-import net.pwall.util.ReaderBuffer;
-
 /**
  * Test various forms of parsing.
  */
 public class TestParse {
 
-    private static final String exampleString = "{\"aaa\":1234}";
+    private static final String exampleString = "{\"aaa\":1234,\"bbb\":true}";
 
     @Test
     public void test() throws IOException {
@@ -28,13 +26,19 @@ public class TestParse {
         JSONValue value = object.get("aaa");
         assertTrue(value instanceof JSONInteger);
         assertEquals(new JSONInteger(1234), value);
+        value = object.get("bbb");
+        assertTrue(value instanceof JSONBoolean);
+        assertEquals(JSONBoolean.TRUE, value);
 
-        result = JSON.parse(new ReaderBuffer(new StringReader(exampleString)));
+        result = JSON.parse(new StringReader(exampleString));
         assertTrue(result instanceof JSONObject);
         object = (JSONObject)result;
         value = object.get("aaa");
         assertTrue(value instanceof JSONInteger);
         assertEquals(new JSONInteger(1234), value);
+        value = object.get("bbb");
+        assertTrue(value instanceof JSONBoolean);
+        assertEquals(JSONBoolean.TRUE, value);
     }
 
 }
